33441d51822a3efc73c6fb465d644dcca344c6ed
Ole Martin Gjerde <gjerde.olem@gmail.com>
2022-10-04 09:25:40 +0200

    add controllers for tables

---
+++ Models/Domain/Event.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Alumni_Network_Portal_BE.Models.Domain
+{
+    public class Event
+    {
+        // PK
+        public int Id { get; set; }
+        // Fields
+        [MaxLength(255)]
+        [Required]
+        public string Name { get; set; }
+        [MaxLength(255)]
+        public string? Description { get; set; } //Nullable
+        [Required]
+        public DateTime LastUpdated { get; set; }
+        [Required]
+        public DateTime StartTime { get; set; }
+        [Required]
+        public DateTime EndTime { get; set; }
+        [Required]
+        public bool AllowGuests { get; set; }
+
+        // Relationships
+    }
+}
