6f637a6205ecf6cf118e70e820537125aaf3230b
Fredrik Fauskanger <ffauskanger@gmail.com>
2022-10-06 10:47:09 +0200

    fix: removed allowcredentials from cors

--- Program.cs
+++ Program.cs
@@ -104,8 +104,7 @@ builder.Services.AddCors(options =>
         builder
         .AllowAnyOrigin()
         .AllowAnyMethod()
-        .AllowAnyHeader()
-        .AllowCredentials();
+        .AllowAnyHeader();
     });
 });
 
