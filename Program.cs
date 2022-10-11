363e5744657970580ce57310aa5dc5d3b5a289ab
Fredrik Fauskanger <ffauskanger@gmail.com>
2022-10-11 11:30:22 +0200

    feat: possible deployment

--- Program.cs
+++ Program.cs
@@ -15,6 +15,65 @@ using Alumni_Network_Portal_BE.Services.EventServices;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var configurationBuilder = new ConfigurationBuilder()
+                            .SetBasePath(builder.Environment.ContentRootPath)
+                            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                            .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true)
+                            .AddEnvironmentVariables();
+
+
+//FIXME: Testing of deployment
+builder.Services.AddDbContext<AlumniNetworkDbContext>(
+    opt => {
+        if(builder.Environment.EnvironmentName == "Development")
+        {
+            opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+        }
+        else
+        {
+            opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+        }
+    });
+
+using (var serviceProvider = builder.Services.BuildServiceProvider())
+{
+    var dbContext = serviceProvider.GetRequiredService<AlumniNetworkDbContext>();
+    dbContext.Database.Migrate();
+}
+
+var defaultConnectionString = string.Empty;
+
+if (builder.Environment.EnvironmentName == "Development")
+{
+    defaultConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+}
+else
+{
+    // Use connection string provided at runtime by Heroku.
+    var connectionUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+    connectionUrl = connectionUrl.Replace("postgres://", string.Empty);
+    var userPassSide = connectionUrl.Split("@")[0];
+    var hostSide = connectionUrl.Split("@")[1];
+
+    var user = userPassSide.Split(":")[0];
+    var password = userPassSide.Split(":")[1];
+    var host = hostSide.Split("/")[0];
+    var database = hostSide.Split("/")[1].Split("?")[0];
+
+    defaultConnectionString = $"Host={host};Database={database};Username={user};Password={password};SSL Mode=Require;Trust Server Certificate=true";
+}
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+builder.Services.AddScoped(typeof(ITopicService), typeof(TopicService));
+builder.Services.AddScoped(typeof(IUserService), typeof(UserService));
+builder.Services.AddScoped(typeof(IGroupService), typeof(GroupService));
+builder.Services.AddScoped(typeof(IPostService), typeof(PostService));
+builder.Services.AddScoped(typeof(IEventService), typeof(EventService));
+
+
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
@@ -38,20 +97,6 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             ValidAudience = "account",
         };
     });
-
-
-
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddDbContext<AlumniNetworkDbContext>(
-    opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddAutoMapper(typeof(Program).Assembly);
-builder.Services.AddScoped(typeof(ITopicService), typeof(TopicService));
-builder.Services.AddScoped(typeof(IUserService), typeof(UserService));
-builder.Services.AddScoped(typeof(IGroupService), typeof(GroupService));
-builder.Services.AddScoped(typeof(IPostService), typeof(PostService));
-builder.Services.AddScoped(typeof(IEventService), typeof(EventService));
-
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -111,6 +156,7 @@ builder.Services.AddCors(options =>
         .AllowAnyHeader();
     });
 });
+
 
 
 
