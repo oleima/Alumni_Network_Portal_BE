63b1a063a4ecf4c9f5fb0b8ff64c165ec37315f1
Fredrik Fauskanger <ffauskanger@gmail.com>
2022-10-11 15:31:03 +0200

    fix: fix datetime for entites for postgres

--- Models/AlumniNetworkDbContext.cs
+++ Models/AlumniNetworkDbContext.cs
@@ -35,7 +35,34 @@ namespace Alumni_Network_Portal_BE.Models
                             property.SetValueConverter(boolConverter);
                     }
                 }
-            
+
+            var dateTimeConverter = new ValueConverter<DateTime, DateTime>(
+            v => v.ToUniversalTime(),
+            v => DateTime.SpecifyKind(v, DateTimeKind.Utc));
+
+            var nullableDateTimeConverter = new ValueConverter<DateTime?, DateTime?>(
+                v => v.HasValue ? v.Value.ToUniversalTime() : v,
+                v => v.HasValue ? DateTime.SpecifyKind(v.Value, DateTimeKind.Utc) : v);
+
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                if (entityType.IsKeyless)
+                {
+                    continue;
+                }
+
+                foreach (var property in entityType.GetProperties())
+                {
+                    if (property.ClrType == typeof(DateTime))
+                    {
+                        property.SetValueConverter(dateTimeConverter);
+                    }
+                    else if (property.ClrType == typeof(DateTime?))
+                    {
+                        property.SetValueConverter(nullableDateTimeConverter);
+                    }
+                }
+            }
 
             // Seeddata
 
