bb181aadc5b3e4c5dc6f2bd289dc4bfc6dac0947
Ole Martin Gjerde <gjerde.olem@gmail.com>
2022-10-12 14:07:39 +0200

    fix: tailor endpoints to frontend

--- Models/DTOs/GroupDTO/GroupReadDTO.cs
+++ Models/DTOs/GroupDTO/GroupReadDTO.cs
@@ -1,5 +1,7 @@
 ﻿using Alumni_Network_Portal_BE.Models.Domain;
+using Alumni_Network_Portal_BE.Models.DTOs.EventDTO;
 using Alumni_Network_Portal_BE.Models.DTOs.PostDTO;
+using Alumni_Network_Portal_BE.Models.DTOs.UserDTO;
 using System.ComponentModel.DataAnnotations;
 
 namespace Alumni_Network_Portal_BE.Models.DTOs.GroupDTO
@@ -11,7 +13,7 @@ namespace Alumni_Network_Portal_BE.Models.DTOs.GroupDTO
         public string? Body { get; set; }
         public bool IsPrivate { get; set; }
         public ICollection<PostReadDTO>? Posts { get; set; } //One-Many
-        public List<int>? Users { get; set; }  //Many-Many
-        public List<int>? Events { get; set; }//Many-Many
+        public ICollection<UserPostReadDTO>? Users { get; set; } //Many-Many
+        public ICollection<EventReadDTO>? Events { get; set; } //Many-Many
     }
 }
